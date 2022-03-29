4cc06cd67a843c0e4cee3a3d91ebe12e812ee15f
Shawn Smith <sds.smith24@gmail.com>
2022-03-29 09:13:49 -0400

    create form in ContactForm return statement

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -11,8 +11,13 @@ export const ContactForm = ({
 }) => {
 
 
-  
+
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type='text' id='name'>Contact Name</input>
+      <input type='tel' id='phone'>Contact Phone Number</input>
+      <input type='email' id='email' >Contact Email Address</input>
+      <button type='submit' id='submit' />
+    </form>
   );
 };
