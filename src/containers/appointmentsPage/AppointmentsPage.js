3576c100d41ab47bb808aff73a8abfd9f5726799
Shawn Smith <sds.smith24@gmail.com>
2022-03-29 13:31:13 -0400

    import useState, declare state variables in AppointmentsPage

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AppointmentsPage = (props) => {
 
   const appointments = props.appointments 
   const contacts = props.contacts 
   const addAppointment = props.addAppointment
+
+    const [title, setTitle] = useState('')
+    const [contact, setContact] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault();
