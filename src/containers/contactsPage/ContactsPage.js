8d7c9c5748253afb305d30f0a39e6571102ee6b0
Shawn Smith <sds.smith24@gmail.com>
2022-03-29 09:16:13 -0400

    import ContactForm and add props in ContactsPage

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { ContactForm } from "../../components/contactForm/ContactForm";
 
-export const ContactsPage = () => {
+export const ContactsPage = (contacts, addContact) => {
   /*
   Define state variables for 
   contact info and duplicate check
